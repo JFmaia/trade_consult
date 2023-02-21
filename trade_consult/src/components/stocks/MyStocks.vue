55ee732dd8c5fc4df95e8b21130e95e04806ff43
JFmaia <jfmaia741@gmail.com>
2023-02-21 18:07:08 -0300

    feat: Estruturando component stocks

--- trade_consult/src/components/stocks/MyStocks.vue
+++ trade_consult/src/components/stocks/MyStocks.vue
@@ -1,10 +1,23 @@
 <template>
-    <h1>Lista de ações</h1>
+    <v-layout row wrap>
+        <Stock v-for="stock in stocks" :key="stock.id"></Stock>
+    </v-layout>
 </template>
 
 <script lang="ts">
+import Stock from './MyStock.vue'
 export default {
-    
+    components:{Stock},
+    data(){
+        return{
+            stocks:[
+                { id: 1, name: "BMW", price: 110},
+                { id: 2, name: "Google", price: 200},
+                { id: 3, name: "Apple", price: 280},
+                { id: 4, name: "Twitter", price: 83},
+            ]
+        }
+    }
 }
 </script>
 
