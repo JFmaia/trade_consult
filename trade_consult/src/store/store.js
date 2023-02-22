396c6ee53d383e21abeb935bfd732d1a59786bad
JFmaia <jfmaia741@gmail.com>
2023-02-22 10:30:07 -0300

    feat: Registrando modulo na store.

--- trade_consult/src/store/store.js
+++ trade_consult/src/store/store.js
@@ -3,19 +3,13 @@ import Vuex from 'vuex'
 
 //Modules
 import stocks from '@/store/modules/stocks'
+import portfolio from '@/store/modules/portfolio'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
   modules: {
-    stocks
+    stocks,
+    portfolio,
   }
 })
