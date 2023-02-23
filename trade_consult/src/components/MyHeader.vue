aff11da58060b38573dfcd484ae5493cea7f5c84
JFmaia <jfmaia741@gmail.com>
2023-02-23 17:07:40 -0300

    feat: Adicionado filtros.

--- trade_consult/src/components/MyHeader.vue
+++ trade_consult/src/components/MyHeader.vue
@@ -25,14 +25,14 @@
             </v-menu>
             <v-layout align-center>
                 <span class="text-uppercase grey--text text--darken-2">
-                    Saldo: {{ funds }}
+                    Saldo: {{ funds | currency }}
                 </span>
             </v-layout>
         </v-toolbar-items>
     </v-toolbar>
 </template>
 
-<script lang="ts">
+<script>
 export default {
     computed:{
         funds(){
